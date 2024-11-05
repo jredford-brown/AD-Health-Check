e16903cf6207a7c37dd1775b597f1a067013dc17
jredford-brown <j.redford-brown@jredfordmcp.network>
2024-11-05 11:25:02 +0000

    Big changes to dcdiag, trying to resolve dns latency module, changed txt file name

--- AD Health Check Script.ps1
+++ AD Health Check Script.ps1
@@ -3,21 +3,20 @@ try {
     Import-Module $scriptDir\Get-ADTestPorts.ps1
     Import-Module $scriptDir\Get-ADEthernetForwarderConfiguration.ps1
     Import-Module $scriptDir\Get-ADLDAPConfiguration.ps1
-    Import-Module $scriptDir\Get-NTPandDNS.ps1
+    Import-Module $scriptDir\Get-NTP.ps1
     Import-Module $scriptDir\Get-DNSLatency.ps1
     Import-Module $scriptDir\Get-SitesServices.ps1
     Import-Module ActiveDirectory
 }
 catch {
-    throw "A fatal exception has occured while importing modules.`n $_.ScriptStackTrace"
+    throw "A fatal exception has occured while importing modules. $_.ScriptStackTrace"
 }
 
 try {
-    $forwarders = Get-DnsServerForwarder
     $query = "www.bing.com"
     $sitesAndHosts = Get-ADDomainController -Filter *
     $hostName = [System.Net.Dns]::GetHostEntry($env:computerName).HostName
-    $transcriptPath = "${env}TTech AD Health Check - $(Get-Date -Format "ddMMyy.HHmm").txt"    
+    $transcriptPath = "${env}Active Directory Health Check - $(Get-Date -Format "ddMMyy.HHmm").txt"
 }
 catch {
     Write-Warning "An error occured during variable data collection. $_.ScriptStackTrace"
@@ -29,7 +28,7 @@ try {
     Write-Host "=== System Information ==="
     Get-ADEthernetForwarderConfiguration
     Get-NTPandDNS
-    Write-Output ""    
+    Write-Output ""
 }
 catch {
     Write-Warning "An error occured while building system info. $_.ScriptStackTrace"
@@ -37,29 +36,22 @@ catch {
 
 try {
     $dcdiagTests = @(
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:dns"; Heading = "DNS Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:machineaccount"; Heading = "Machine Account Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:services"; Heading = "Services Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:netlogons"; Heading = "Netlogons Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:replications"; Heading = "Replications Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:fsmocheck"; Heading = "FSMO Check Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:advertising"; Heading = "Self Advertising Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:CheckSDRefDom"; Heading = "Security Descriptor Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:CheckSecurityError /ReplSource"; Heading = "Security Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:Connectivity"; Heading = "DSA Test" }
-        [PSCustomObject]@{ Command = "dcdiag /a /v /test:cutoffservers"; Heading = "Isolated DC Test" }
+        [PSCustomObject]@{ Command = "dcdiag /test:dns"; Heading = "=== DNS Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:machineaccount"; Heading = "=== Machine Account Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:services"; Heading = "=== Services Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:netlogons"; Heading = "=== Netlogons Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:replications"; Heading = "=== Replications Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:fsmocheck"; Heading = "=== FSMO Check Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:advertising"; Heading = "=== Self Advertising Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:CheckSDRefDom"; Heading = "=== Security Descriptor Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:CheckSecurityError /ReplSource"; Heading = "=== Security Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:Connectivity"; Heading = "=== DSA Test ===" }
+        [PSCustomObject]@{ Command = "dcdiag /test:cutoffservers"; Heading = "=== Isolated DC Test ===" }
     )
     
-    Write-Host "=== DCDIAG Tests ==="
     foreach ($test in $dcdiagTests) {
-        $output = dcdiag /a $test.Command | Select-String -Pattern 'DC:|failed' -Context 1
-        if ($output) {
-            Write-Host "$($test.Heading):"
-            Write-Output ""
-            $output | Out-File -FilePath $fileVar -Append
-        } else {
-            Write-Host "$($test.Heading): No issues to report.`n"
-        }
+        $output = dcdiag /a /v $test.Command /s $hostName
+        Write-Host "$($test.Heading):`n$output"
     }
     Write-Output ""
 }
@@ -85,7 +77,7 @@ try {
             Write-Host "No NS records found for zone '$zone'."
         }
     }
-    Write-Output ""    
+    Write-Output ""
 }
 catch {
     Write-Warning "An error occured while checking Name Server records. $_.ScriptStackTrace"
@@ -93,8 +85,8 @@ catch {
 
 try {
     Write-Host "=== Forwarder Response Time ==="
-    Get-DNSLatency($forwarders, $query)
-    Write-Output ""    
+    Get-DNSLatency($query)
+    Write-Output ""
 }
 catch {
     Write-Warning "An error occured while checking forwarder latency. $_.ScriptStackTrace"
@@ -103,7 +95,7 @@ catch {
 try {
     Write-Host "=== Test Connectivity ==="
     Get-ADTestPorts
-    Write-Output ""    
+    Write-Output ""
 }
 catch {
     Write-Warning "An error occured while checking port connectivity. $_.ScriptStackTrace"
@@ -112,7 +104,7 @@ catch {
 try {
     Write-Host "=== Replication Status ==="
     Get-SitesServices($sitesAndHosts)
-    Write-Output ""    
+    Write-Output ""
 }
 catch {
     Write-Warning "An error occured while collecting replication status. $_.ScriptStackTrace"
@@ -120,7 +112,7 @@ catch {
 
 try {
     Write-Host "=== LDAP Configuration ==="
-    Get-ADLDAPConfiguration($hostName)    
+    Get-ADLDAPConfiguration($hostName)
 }
 catch {
     Write-Warning "An error occured while collecting LDAP configuration. $_.ScriptStackTrace"
--- Get-DNSLatency.ps1
+++ Get-DNSLatency.ps1
@@ -1,31 +1,32 @@
 ﻿function Get-DNSLatency{
     param(
-        [string]$Server,
-        [string]$Query
+        [string]$query
     )
 
-    foreach ($forwarder in $Server) {
+    $forwarders = Get-DnsServerForwarder
+
+    foreach ($forwarder in $forwarders) {
         foreach ($nsRecord in $forwarder.IPAddress) {
             if ($nsRecord -match '\d+\.\d+\.\d+\.\d+') {
                 $totalMilliseconds = 0
                 for ($i = 1; $i -le 10; $i++) {
-                    $measurement = (Measure-Command {Resolve-DnsName www.bing.com -Server $nsRecord -Type A}).TotalMilliseconds
+                    $measurement = (Measure-Command {Resolve-DnsName $query -Server $nsRecord -Type A}).TotalMilliseconds
                     $totalMilliseconds += $measurement
                 }
                 $averageMilliseconds = $totalMilliseconds / 10
-                Write-Output "Average query time for $nsRecord $averageMilliseconds milliseconds" -ForegroundColor Black
+                Write-Output "Average query time for $nsRecord $($averageMilliseconds)ms"
             } else {
                 $resolvedAddress = (Resolve-DnsName -Name $nsRecord -ErrorAction SilentlyContinue).IPAddress
                 if ($resolvedAddress) {
                     $totalMilliseconds = 0
                     for ($i = 1; $i -le 10; $i++) {
-                        $measurement = (Measure-Command {Resolve-DnsName www.bing.com -Server $nsRecord -Type A}).TotalMilliseconds
+                        $measurement = (Measure-Command {Resolve-DnsName $query -Server $nsRecord -Type A}).TotalMilliseconds
                         $totalMilliseconds += $measurement
                     }
                     $averageMilliseconds = $totalMilliseconds / 10
-                    Write-Output "Average query time for $resolvedAddress $averageMilliseconds milliseconds" -ForegroundColor Black
+                    Write-Output "Average query time for $resolvedAddress $($averageMilliseconds)ms"
                 } else {
-                    Write-Output "Failed to resolve DNS forwarder address: $nsRecord" -ForegroundColor Red
+                    Write-Output "Failed to resolve DNS forwarder address: $nsRecord"
                     continue
                 }
             }
