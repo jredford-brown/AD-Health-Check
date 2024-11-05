002cefa7adf0ab26e605dc47654a31aeaf1d9a9f
jredford-brown <j.redford-brown@jredfordmcp.network>
2024-11-05 12:17:42 +0000

    Changed dcdiag forloop to reflect right commands, hopefully

--- AD Health Check Script.ps1
+++ AD Health Check Script.ps1
@@ -50,7 +50,7 @@ try {
     )
     
     foreach ($test in $dcdiagTests) {
-        $output = dcdiag /a /v $test.Command /s:$hostName
+        $output = $test.Command /a /v /s:$hostName
         Write-Host "$($test.Heading)`n$output"
     }
     Write-Output ""
