1120c0c89aaac0244299391e4f61e884102076a8
jredford-brown <j.redford-brown@jredfordmcp.network>
2024-11-06 09:35:48 +0000

    Moved DNS into another module

--- Get-NTP.ps1
+++ Get-NTP.ps1
@@ -1,25 +1,11 @@
-﻿function Get-NTPandDNS {
+﻿function Get-NTP {
     $ntpSettings = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters"
     $ntpServer = $ntpSettings.NtpServer
     $type = $ntpSettings.Type
-    $ipConfig = Get-NetIPConfiguration -InterfaceAlias $nic.InterfaceAlias
+    
 
     Write-Host "=== NTP Configuration ==="
     Write-Output "NTP Server: $ntpServer"
     Write-Output "Type: $type"
     Write-Output ""
-
-    Write-Host "=== Network Adapter Configuration ==="
-    Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | ForEach-Object {
-        $nic = $_
-        $dnsServers = $ipConfig.DnsServer.ServerAddresses
-        Write-Output "Interface Name: $($nic.Name)"
-        if ($dnsServers.Count -gt 0) {
-            Write-Output "DNS Servers:"
-            $dnsServers | ForEach-Object { Write-Output "  $_" }
-        } else {
-            Write-Output "DNS Servers: N/A"
-        }
-        Write-Output ""
-    }
 }
